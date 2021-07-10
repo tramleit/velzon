b0d28fdec3a916e3d934fe40b0a1b61166a6c51a
josuapsianturi <josuasianturi561@gmail.com>
2021-07-10 13:53:26 +0700

    display subtotal

--- resources/views/livewire/cart-product.blade.php
+++ resources/views/livewire/cart-product.blade.php
@@ -1,7 +1,7 @@
 {{-- @dump(Cart::content()) --}}
-<main id="main">
-    <div class="text-2xl">Shopping Cart</div>
-    <hr class=" ">
+<main id="main" class="p-5 flex space-x-3 bg-gray-200">
+<div class="w-9/12 bg-white p-5">
+    <div class="text-2xl mb-5">Shopping Cart</div>
 
     <div class="">
     @if (Session::has('success_message'))
@@ -15,10 +15,11 @@
     <ul>
         @foreach (Cart::content() as $item)
         <li>
-            <div class="flex checkoutProduct" style="margin-top: 20px; margin-bottom: 20px" >
+            <hr class="border-gray-300">
+            <div class="flex checkoutProduct" style=" margin-bottom: 20px" >
                 <img src="{{ asset('assets/images/products') }}/{{ $item->model->image }}" alt="{{ $item->model->name }}" class="object-contain checkoutproduct__image" style="width: 180px; height: 180px;" />
 
-                <div class="checkoutproduct__info" style="padding-left: 20px;" >
+                <div class="checkoutproduct__info mt-8 " style="padding-left: 20px;" >
                     <a href="{{ route('product.details', ['slug' => $item->model->slug ]) }}" class="checkoutproduct__title" style="" >{{ $item->model->name }}</a>
                     <p class="checkoutproduct__price">
                         <small> $</small>
@@ -39,4 +40,18 @@
     @endif
 
     </div>
+</div>
+
+<div class="w-3/12 p-5 bg-white max-h-44">
+    <div class="text-xl">
+        Subtotal (
+        <span>
+        @if (Cart::count() > 0)
+            {{ Cart::count() }}
+        @endif </span>  items):
+        <span class=""><b> $ {{ Cart::subtotal() }}</b></span>
+
+    </div>
+    <div class="border text-sm py-1 rounded-md bg-yellow-400 border-yellow-500 text-center mt-6">Proceed to checkout</div>
+</div>
 </main>
