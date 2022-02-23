3d887cec9933ce68af34c76389c1fd810d50ca80
Josua Putra Sianturi <josuapsianturi@gmail.com>
2022-02-23 15:13:20 +0700

    Add validation to create category form

--- app/Http/Livewire/Admin/AdminAddCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminAddCategoryComponent.php
@@ -11,24 +11,41 @@ class AdminAddCategoryComponent extends Component
     public $name;
     public $slug;
 
-    public function generateslug()
-    {
-        $this->slug = Str::slug($this->name);
-    }
-
-    public function storeCategory()
-    {
-        $category = new Category();
-        $category->name = $this->name;
-        $category->slug = $this->slug;
-        $category->save();
-
-        session()->flash('message', 'Category succesfully created');
-        return redirect()->to('/admin/category/add');
-    }
-
     public function render()
     {
         return view('livewire.admin.admin-add-category-component')->layout('layouts.base');
     }
+
+    public function generateSlug()
+    {
+        $this->slug = Str::slug($this->name, '-');
+    }
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            'name' => 'required',
+            'slug'  => 'required|unique:categories'
+        ]);
+    }
+
+    protected $rules = [
+        'name' => 'required|min:6',
+        'slug' => 'required|unique:categories',
+    ];
+
+    public function addCategory()
+    {
+        $this->validate($this->rules);
+
+        Category::create([
+            'name' => $this->name,
+            'slug' => $this->slug,
+        ]);
+
+        return redirect('/admin/categories')->with('message', 'Category successfully added.');
+    }
+
+
+
 }
