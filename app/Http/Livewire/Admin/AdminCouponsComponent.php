4c9f39fcfa1577f99d5ea0af583516b2b5d1303f
Josua Putra Sianturi <josuapsianturi@gmail.com>
2022-02-25 15:45:09 +0700

    Show all coupons

---
+++ app/Http/Livewire/Admin/AdminCouponsComponent.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Models\Coupon;
+use Livewire\Component;
+
+class AdminCouponsComponent extends Component
+{
+    public function render()
+    {
+        $coupons = Coupon::all();
+        return view('livewire.admin.admin-coupons-component', ['coupons' => $coupons])->layout('layouts.base');
+    }
+
+}
