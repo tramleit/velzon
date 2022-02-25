475d8f92bff2b5e3be5131f8437989e3c40ed31d
Josua Putra Sianturi <josuapsianturi@gmail.com>
2022-02-25 09:01:17 +0700

    Move product from wishlist to cart

--- resources/views/livewire/cart-count-component.blade.php
+++ resources/views/livewire/cart-count-component.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ ('cart') }}"  class="flex items-center" style="color: white">
+<a href="{{ route('product.cart') }}"  class="flex items-center" style="color: white">
     <x-heroicon-o-shopping-cart class="w-8 h-8" />
     <div class="" style="font-size: 14px; font-weight: 800; margin-left: 10px; margin-right: 10px">
         @if ( Cart::instance('cart')->count() > 0 )
