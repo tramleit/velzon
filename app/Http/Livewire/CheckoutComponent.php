fc913eb591d2d3e3cd13f1043490c936f5074dc0
Josua Putra Sianturi <josuapsianturi@gmail.com>
2022-03-01 12:04:46 +0700

    Fix checkout page and add checkout properties

--- app/Http/Livewire/CheckoutComponent.php
+++ app/Http/Livewire/CheckoutComponent.php
@@ -6,6 +6,31 @@ use Livewire\Component;
 
 class CheckoutComponent extends Component
 {
+    // public $ship_to_different;
+    // Billing Address
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $mobile;
+    public $line1;
+    public $line2;
+    public $country;
+    public $city;
+    public $zipcode;
+    public $province;
+
+    // Shipping Address
+    public $s_firstname;
+    public $s_lastname;
+    public $s_email;
+    public $s_mobile;
+    public $s_line1;
+    public $s_line2;
+    public $s_country;
+    public $s_province;
+    public $s_city;
+    public $s_zipcode;
+
     public function render()
     {
         return view('livewire.checkout-component')->layout('layouts.base');
--- resources/views/livewire/checkout-component.blade.php
+++ resources/views/livewire/checkout-component.blade.php
@@ -1,77 +1,134 @@
 <!-- component -->
-<section class="px-4 mt-5 antialiased text-gray-600 bg-gray-100">
+<section class="px-4 antialiased text-gray-600">
     <div class="flex flex-col justify-center h-full">
-        <!-- Add New Products -->
-        <div class="w-full max-w-3xl mx-auto bg-white border border-gray-200 rounded-sm shadow-lg">
-            <header class="flex justify-center px-5 py-4 border-b border-gray-100">
-                <h2 class="font-semibold text-gray-800">Billing Address</h2>
-            </header>
-
-            <!-- form -->
-            <div class="p-5">
-            <form class="space-y-5">
-                <p class="flex justify-around">
-                    <label for="fname">first name<span>*</span></label>
-                    <input id="fname" type="text" name="fname" value="" placeholder="Your name">
-                </p>
-                <p class="flex justify-around">
-                    <label for="lname">last name<span>*</span></label>
-                    <input id="lname" type="text" name="lname" value="" placeholder="Your last name">
-                </p>
-                <p class="flex justify-around">
-                    <label for="email">Email Addreess:</label>
-                    <input id="email" type="email" name="email" value="" placeholder="Type your email">
-                </p>
-                <p class="flex justify-around">
-                    <label for="phone">Phone number<span>*</span></label>
-                    <input id="phone" type="number" name="phone" value="" placeholder="10 digits format">
-                </p>
-                <p class="flex justify-around">
-                    <label for="add">Address:</label>
-                    <input id="add" type="text" name="add" value="" placeholder="Street at apartment number">
-                </p>
-                <p class="flex justify-around">
-                    <label for="country">Country<span>*</span></label>
-                    <input id="country" type="text" name="country" value="" placeholder="United States">
-                </p>
-                <p class="flex justify-around">
-                    <label for="zip-code">Postcode / ZIP:</label>
-                    <input id="zip-code" type="number" name="zip-code" value="" placeholder="Your postal code">
-                </p>
-                <p class="flex justify-around">
-                    <label for="city">Town / City<span>*</span></label>
-                    <input id="city" type="text" name="city" value="" placeholder="City name">
-                </p>
-                <p class="flex justify-around fill-wife">
-                    <label class="checkbox-field">
-                        <input name="create-account" id="create-account" value="forever" type="checkbox">
-                        <span>Create an account?</span>
-                    </label>
-                    <label class="checkbox-field">
-                        <input name="different-add" id="different-add" value="forever" type="checkbox">
+        <!--- Checkout -->
+        <header class="">
+            <h1 class="text-3xl font-bold text-center text-green-600">Checkout</h1>
+        </header>
+        <div class="w-full max-w-3xl mx-auto mt-10 bg-white border border-gray-200 rounded-sm shadow-lg">
+            <form>
+            <!-- Form Billing Address -->
+            <div x-data="{ open: false }" class="">
+            <div class="">
+                <div class="flex justify-center px-5 py-4 border-b border-gray-100">
+                    <h2 class="font-semibold text-gray-800">Billing Address</h2>
+                </div>
+                <div class="space-y-5">
+                    <div class="flex justify-around">
+                        <label for="fname">first name<span>*</span></label>
+                        <input type="text" name="fname" value="" placeholder="Your name" wire:model="firstname">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="lname">last name<span>*</span></label>
+                        <input type="text" name="lname" value="" placeholder="Your last name" wire:model="lastname">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="email">Email Addreess:</label>
+                        <input  type="email" name="email" value="" placeholder="Type your email" wire:model="email">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="phone">Phone number<span>*</span></label>
+                        <input type="number" name="phone" value="" placeholder="10 digits format" wire:model="mobile">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="add">Line 1:</label>
+                        <input type="text" name="add" value="" placeholder="Street at apartment number" wire:model="line1">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="add">Line 2:</label>
+                        <input type="text" name="add" value="" placeholder="Street at apartment number" wire:model="line2">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="country">Country<span>*</span></label>
+                        <input type="text" name="country" value="" placeholder="United States" wire:model="country">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="city">Province<span>*</span></label>
+                        <input type="text" name="province" value="" placeholder="Province name" wire:model="province">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="city">Town / City<span>*</span></label>
+                        <input type="text" name="city" value="" placeholder="City name" wire:model="city">
+                    </div>
+                    <div class="flex justify-around" >
+                        <label for="zip-code">Postcode / ZIP:</label>
+                        <input type="number" name="zip-code" value="" placeholder="Your postal code" wire:model="zipcode">
+                    </div>
+                    <div class="flex justify-around fill-wife">
                         <span>Ship to a different address?</span>
-                    </label>
-                </p>
-            </form>
+                        <button @click.prevent="open = !open" class="px-4 py-2 text-white bg-gray-700 rounded shadow" >Open</button>
+                    </div>
+                </div>
             </div>
-            <!-- End Form -->
-
+            <!-- End Form Billing Address -->
+            <!-- Form Shipping Address -->
+            <div x-show="open" x-transition.opacity>
+                <div  class="my-3 border border-gray-500"></div>
+                <div class="flex justify-center px-5 py-4 border-b border-gray-100">
+                    <h2 class="font-semibold text-gray-800">Shipping Address</h2>
+                </div>
+                <div class="space-y-5">
+                    <div class="flex justify-around">
+                        <label for="fname">first name<span>*</span></label>
+                        <input  type="text" name="fname" value="" placeholder="Your name" wire:model="s_firstname">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="lname">last name<span>*</span></label>
+                        <input type="text" name="lname" value="" placeholder="Your last name" wire:model="s_lastname">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="email">Email Addreess:</label>
+                        <input type="email" name="email" value="" placeholder="Type your email" wire:model="s_email">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="phone">Phone number<span>*</span></label>
+                        <input type="number" name="phone" value="" placeholder="10 digits format" wire:model="s_mobile">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="add">Line 1:</label>
+                        <input type="text" name="add" value="" placeholder="Street at apartment number" wire:model="s_line1">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="add">Line 2:</label>
+                        <input type="text" name="add" value="" placeholder="Street at apartment number" wire:model="s_line2">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="country">Country<span>*</span></label>
+                        <input type="text" name="country" value="" placeholder="United States" wire:model="s_country">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="city">Province<span>*</span></label>
+                        <input type="text" name="province" value="" placeholder="Province name" wire:model="s_province">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="city">Town / City<span>*</span></label>
+                        <input type="text" name="city" value="" placeholder="City name" wire:model="s_city">
+                    </div>
+                    <div class="flex justify-around">
+                        <label for="zip-code">Postcode / ZIP:</label>
+                        <input type="number" name="zip-code" value="" placeholder="Your postal code" wire:model="s_zipcode">
+                    </div>
+                </div>
+            </div>
+            </div>
+            <!--- End Form Shipping Address -->
+            <div class="my-3 border border-gray-500"></div>
             <!-- Payment and Shipping Method -->
-            <div class="p-5 mt-10">
+            <div class="p-5 mt-3">
                 <!-- Payment Method -->
                 <div class="space-y-5">
-                    <h4 class="text-center text-xl">Payment Method</h4>
-                    <p class=""><span class="">Check / Money order</span></p>
-                    <p class=""><span class="">Credit Cart (saved)</span></p>
+                    <h4 class="text-xl text-center">Payment Method</h4>
+                    <div class=""><span class="">Check / Money order</span></div>
+                    <div class=""><span class="">Credit Cart (saved)</span></div>
                     <div class="">
                         <label class="payment-method">
-                            <input name="payment-method" id="payment-method-bank" value="bank" type="radio" class="">
-                            <span class="font-semibold">Direct Bank Transder</span>
-                            <div class="">But the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</div>
+                            <input name="payment-method" id="payment-method-bank" value="cod" type="radio" class="">
+                            <span class="font-semibold">Cash On Delivery</span>
+                            <div class="">Order now pay on delivery</div>
                         </label>
                         <label class="">
-                            <input name="payment-method" id="payment-method-visa" value="visa" type="radio">
-                            <span class="font-semibold">visa</span>
+                            <input name="payment-method" id="payment-method-visa" value="card" type="radio">
+                            <span class="font-semibold">Debit / Credit Card</span>
                             <div class="">There are many variations of passages of Lorem Ipsum available</div>
                         </label>
                         <label class="payment-method">
@@ -81,31 +138,30 @@
                             <div class="">card if you don't have a paypal account</div>
                         </label>
                     </div>
-                    <p class=""><span>Grand Total</span> <span class="">$100.00</span></p>
-                    <a href="" class="">Place order now</a>
+                    <div class="">
+                        <div class="flex justify-around font-bold"><span>Grand Total</span> <span class="">$100.00</span></div>
+                    </div>
+                    <div class="flex justify-center">
+                        <button type="submit" class="px-4 py-2 bg-yellow-500 rounded shadow">Place order now</button>
+                    </div>
                 </div>
                 <!-- End Payment Method -->
-
+                <div class="my-3 border border-gray-500"></div>
                 <!-- Shipping Method -->
                 <div class="">
-                    <h4 class="text-center text-xl">Shipping method</h4>
-                    <p class=""><span class="">Flat Rate</span></p>
-                    <p class=""><span class="">Fixed $50.00</span></p>
-                    <div class="">
-                    <h4 class="">Discount Codes</h4>
-                    <div class="flex justify-around my-3">
-                        <label for="coupon-code">Enter Your Coupon code:</label>
-                        <input id="coupon-code" type="text" name="coupon-code" value="" placeholder="">
-                    </div>
+                    <h4 class="text-xl text-center">Shipping method</h4>
+                    <div class=""><span class="">Flat Rate</span></div>
+                    <div class=""><span class="">Fixed $0</span></div>
                     <div class="flex justify-center mt-10">
-                        <button href="#" class=" py-2 px-4 rounded shadow bg-yellow-500">Apply</button>
+                        <button href="#" class="px-4 py-2 bg-yellow-500 rounded shadow ">Apply</button>
                     </div>
                 </div>
                 <!-- End Shipping Method -->
             </div>
             <!-- End Payment Shipping Method -->
+            </form>
         </div>
-
+        <!-- End Checkout -->
     </div>
 </section>
 
