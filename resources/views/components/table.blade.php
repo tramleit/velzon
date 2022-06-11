828eff9547f2cb071f10c05dfd5946f8c6eb320b
Josua Putra Sianturi <josuapsianturi@gmail.com>
2022-06-11 19:17:41 +0700

    Change orders layout to class based components

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,9 @@
+<div class="overflow-x-auto">
+    <table class="w-full mb-4 table-auto">
+        <thead class="text-xs font-semibold text-gray-400 uppercase bg-gray-50">
+            <tr>
+                {{ $header }}
+            </tr>
+        </thead>
+    </table>
+</div>
--- resources/views/livewire/user/user-orders-component.blade.php
+++ resources/views/livewire/user/user-orders-component.blade.php
@@ -14,103 +14,99 @@
                 </div>
                 @endif
 
-                <div class="overflow-x-auto">
-                    <table class="w-full mb-4 table-auto">
-                        <thead class="text-xs font-semibold text-gray-400 uppercase bg-gray-50">
-                            <tr>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-left">Id</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-left">Subtotal</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Discount</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Tax</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Total</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Firstname</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Lastname</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Mobile</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Email</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Zipcode</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Status</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Created At</div>
-                                </th>
-                                <th class="p-2 whitespace-nowrap">
-                                    <div class="font-semibold text-center">Action</div>
-                                </th>
-                            </tr>
-                        </thead>
+                <x-table>
+                    <x-slot:header>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-left">Id</div>
+                        </th>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-left">Subtotal</div>
+                        </th>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-center">Discount</div>
+                        </th>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-center">Tax</div>
+                        </th>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-center">Total</div>
+                        </th>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-center">Firstname</div>
+                        </th>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-center">Lastname</div>
+                        </th>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-center">Mobile</div>
+                        </th>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-center">Email</div>
+                        </th>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-center">Zipcode</div>
+                        </th>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-center">Status</div>
+                        </th>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-center">Created At</div>
+                        </th>
+                        <th class="p-2 whitespace-nowrap">
+                            <div class="font-semibold text-center">Action</div>
+                        </th>
+                    </x-slot>
 
-                        <tbody class="text-sm divide-y divide-gray-100">
-                            @foreach ($orders as $order)
-                            <tr>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="flex items-center">
-                                        <div class="font-medium text-gray-800">{{ $order->id }}</div>
-                                    </div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                        <div class="text-left">{{ $order->subtotal }}</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left">{{ $order->discount }}</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left">{{ $order->tax }}</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left">{{ $order->total }}</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left">{{ $order->firstname }}</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left">{{ $order->lastname }}</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left">{{ $order->mobile }}</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left">{{ $order->email }}</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left">{{ $order->zipcode }}</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left">{{ $order->status }}</div>
-                                </td>
-                                <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left">{{ $order->created_at }}</div>
-                                </td>
-                                <td class="flex items-center p-2 justify-evenly whitespace-nowrap">
-                                    <a href="{{ route('user.orderdetails', ['order_id' => $order->id]) }}" class="px-4 py-2 text-left text-white bg-blue-400 rounded mr-2">Details</a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                    <tbody class="text-sm divide-y divide-gray-100">
+                        @foreach ($orders as $order)
+                        <tr>
+                            <td class="p-2 whitespace-nowrap">
+                                <div class="flex items-center">
+                                    <div class="font-medium text-gray-800">{{ $order->id }}</div>
+                                </div>
+                            </td>
+                            <td class="p-2 whitespace-nowrap">
+                                    <div class="text-left">{{ $order->subtotal }}</div>
+                            </td>
+                            <td class="p-2 whitespace-nowrap">
+                                <div class="text-left">{{ $order->discount }}</div>
+                            </td>
+                            <td class="p-2 whitespace-nowrap">
+                                <div class="text-left">{{ $order->tax }}</div>
+                            </td>
+                            <td class="p-2 whitespace-nowrap">
+                                <div class="text-left">{{ $order->total }}</div>
+                            </td>
+                            <td class="p-2 whitespace-nowrap">
+                                <div class="text-left">{{ $order->firstname }}</div>
+                            </td>
+                            <td class="p-2 whitespace-nowrap">
+                                <div class="text-left">{{ $order->lastname }}</div>
+                            </td>
+                            <td class="p-2 whitespace-nowrap">
+                                <div class="text-left">{{ $order->mobile }}</div>
+                            </td>
+                            <td class="p-2 whitespace-nowrap">
+                                <div class="text-left">{{ $order->email }}</div>
+                            </td>
+                            <td class="p-2 whitespace-nowrap">
+                                <div class="text-left">{{ $order->zipcode }}</div>
+                            </td>
+                            <td class="p-2 whitespace-nowrap">
+                                <div class="text-left">{{ $order->status }}</div>
+                            </td>
+                            <td class="p-2 whitespace-nowrap">
+                                <div class="text-left">{{ $order->created_at }}</div>
+                            </td>
+                            <td class="flex items-center p-2 justify-evenly whitespace-nowrap">
+                                <a href="{{ route('user.orderdetails', ['order_id' => $order->id]) }}" class="px-4 py-2 text-left text-white bg-blue-400 rounded mr-2">Details</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
 
-                    {{ $orders->links() }}
-                </div>
+                {{ $orders->links() }}
+                <x-table/>
             </div>
         </div>
     </div>
